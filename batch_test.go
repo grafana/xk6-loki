9b08e8e127440e9f32a6b47f52ee41678bb56cc2
Christian Haudum <christian.haudum@gmail.com>
2021-12-11 21:19:58 +0100

    Add benchmark tests for protobuf and json encoding
    ```console
    $ go test -bench BenchmarkEncode -benchmem -benchtime 15s
    goos: linux
    goarch: amd64
    pkg: github.com/grafana/xk6-loki
    cpu: Intel(R) Core(TM) i7-8650U CPU @ 1.90GHz
    BenchmarkEncode/encode_protobuf-8         	79969106	       238.6 ns/op	     232 B/op	       2 allocs/op
    BenchmarkEncode/encode_json-8             	 3477244	      5355 ns/op	    3384 B/op	      50 allocs/op
    PASS
    ok  	github.com/grafana/xk6-loki	43.172s
    ```
    
    Signed-off-by: Christian Haudum <christian.haudum@gmail.com>

--- batch_test.go
+++ batch_test.go
@@ -9,7 +9,7 @@ import (
 	"go.k6.io/k6/stats"
 )
 
-func BenchmarkGenerateEntries(b *testing.B) {
+func BenchmarkNewBatch(b *testing.B) {
 	samples := make(chan stats.SampleContainer)
 	state := &lib.State{
 		Samples: samples,
@@ -38,4 +38,46 @@ func BenchmarkGenerateEntries(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		_ = newBatch(ctx, labels, streams, minBatchSize, maxBatchSize)
 	}
+}
+
+func BenchmarkEncode(b *testing.B) {
+	samples := make(chan stats.SampleContainer)
+	state := &lib.State{
+		Samples: samples,
+		VUID:    15,
+	}
+	ctx, cancel := context.WithCancel(context.Background())
+	ctx = lib.WithState(ctx, state)
+
+	defer cancel()
+	defer close(samples)
+	go func() { // this is so that we read the send samples
+		for range samples {
+		}
+	}()
+	faker := gofakeit.New(12345)
+	cardinalities := map[string]int{
+		"app":       5,
+		"namespace": 10,
+		"pod":       100,
+	}
+	streams, minBatchSize, maxBatchSize := 5, 500, 1000
+	labels := newLabelPool(faker, cardinalities)
+
+	b.ReportAllocs()
+	batch := newBatch(ctx, labels, streams, minBatchSize, maxBatchSize)
+
+	b.Run("encode protobuf", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			_, _ = batch.createPushRequest()
+		}
+	})
+
+	b.Run("encode json", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			_, _ = batch.createJSONPushRequest()
+		}
+	})
 }
